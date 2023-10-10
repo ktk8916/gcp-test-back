ed3150ddc6e509ef5998246e0c44a6ff85e883b0
ktk8916 <ktk8916@naver.com>
2023-10-10 10:23:26 +0900

    fail test

--- src/test/java/com/example/back/BackApplicationTests.java
+++ src/test/java/com/example/back/BackApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.back;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class BackApplicationTests {
 
     @Test
     void contextLoads() {
+
+        assertThat(1+1).isZero();
     }
 
 }
